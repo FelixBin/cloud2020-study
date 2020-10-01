41a6d7efbffe2eb121f0bd7c72eb76b4ad385de1
FelixBin <1948728748@qq.com>
2020-10-01 23:03:16 +0800

    eureka

--- cloud-eureka-provider-payment8001/src/main/java/top/wfaceboss/springcloud/dao/PaymentDao.java
+++ cloud-eureka-provider-payment8001/src/main/java/top/wfaceboss/springcloud/dao/PaymentDao.java
@@ -4,9 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import top.wfaceboss.springcloud.entities.Payment;
 
-
 @Mapper
 public interface PaymentDao {
-     int create(Payment payment);
-     Payment getPaymentById(@Param("id") Long id);
+    int create(Payment payment);
+    Payment getPaymentById(@Param("id") Long id);
 }
