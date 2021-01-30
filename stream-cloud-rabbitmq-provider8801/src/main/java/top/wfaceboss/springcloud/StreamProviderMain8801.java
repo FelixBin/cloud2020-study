af3188d4ca27f3a1107d4a720e20c3551447d2a3
FelixBin <1948728748@qq.com>
2021-01-30 12:53:09 +0800

    消息发布端、消息消费端

--- stream-cloud-rabbitmq-provider8801/src/main/java/top/wfaceboss/springcloud/StreamProviderMain8801.java
+++ stream-cloud-rabbitmq-provider8801/src/main/java/top/wfaceboss/springcloud/StreamProviderMain8801.java
@@ -3,12 +3,12 @@ package top.wfaceboss.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaMain7001 {
+@EnableEurekaClient
+public class StreamProviderMain8801 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaMain7001.class, args);
+        SpringApplication.run(StreamProviderMain8801.class, args);
     }
 }
